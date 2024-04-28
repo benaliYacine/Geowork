e85b05944b81c71b3ceba53b9671f23791680072
Benali Yacine <111589591+benaliYacine@users.noreply.github.com>
2024-04-28 23:36:01 +0100

    segemt el role radit el border yeb9a primary hata mor ma tekliki bara bekri kan bel focus 3labiha kan hadak el mouchkil doka segemtou w radit te9der tekliki f ay blasa fel button bah tkhayar ida client wela exper bekri kanet el button div raditha hiya label w hatit radio dakhel l label

--- src/components/formFields/RoleFormField.jsx
+++ src/components/formFields/RoleFormField.jsx
@@ -1,5 +1,10 @@
-import React from 'react';
-import { FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import React from "react";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
@@ -19,46 +24,47 @@ const RoleFormField = ({ control }) => {
               className="flex"
             >
               <div className="flex">
-                <div
+                <Label
                   className={cn(
                     "cursor-pointer space-x-2 flex items-center justify-center px-4 py-2 border text-sm font-medium transition-colors focus-visible:ring-2 focus-visible:ring-offset-2",
                     field.value === "client"
-                      ? "bg-secondary border-primary rounded-l-full"
-                      : "bg-white border-greyDark rounded-l-full"
+                      ? "text-primary bg-secondary border-primary rounded-l-full"
+                      : "text-greyDark bg-white border-greyDark rounded-l-full"
                   )}
+                  htmlFor="client"
                 >
-                  <RadioGroupItem value="client" id="client" />
-                  <Label
+                  <RadioGroupItem
+                    value="client"
+                    id="client"
                     className={cn(
                       field.value === "client"
-                        ? "text-primary"
-                        : "text-greyDark"
+                        ? "border-primary"
+                        : "border-greyDark"
                     )}
-                    htmlFor="client"
-                  >
-                    A Client
-                  </Label>
-                </div>
-                <div
+                  />
+                  A Client
+                </Label>
+
+                <Label
                   className={cn(
                     "cursor-pointer flex items-center space-x-2 justify-center px-4 py-2 border-t border-b border-r text-sm font-medium transition-colors focus-visible:ring-2 focus-visible:ring-offset-2",
                     field.value === "expert"
-                      ? "bg-secondary border-primary rounded-r-full"
-                      : "bg-white border-greyDark rounded-r-full"
+                      ? "text-primary bg-secondary border-primary rounded-r-full"
+                      : "text-greyDark bg-white border-greyDark rounded-r-full"
                   )}
+                  htmlFor="expert"
                 >
-                  <RadioGroupItem value="expert" id="expert" />
-                  <Label
+                  <RadioGroupItem
+                    value="expert"
+                    id="expert"
                     className={cn(
                       field.value === "expert"
-                        ? "text-primary"
-                        : "text-greyDark"
+                        ? "border-primary"
+                        : "border-greyDark"
                     )}
-                    htmlFor="expert"
-                  >
-                    An Expert
-                  </Label>
-                </div>
+                  />
+                  An Expert
+                </Label>
               </div>
             </RadioGroup>
           </div>
