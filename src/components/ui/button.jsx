c412f533bd0ccca182f53de004b6f76d7862baf2
Benali Yacine <111589591+benaliYacine@users.noreply.github.com>
2024-05-17 15:06:34 +0100

    zedt loading state fe l button w zedt rating f cancel job

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva } from "class-variance-authority";
-
+import { LoaderCircle } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
@@ -38,14 +38,29 @@ const buttonVariants = cva(
 );
 
 const Button = React.forwardRef(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    {
+      className,
+      children,
+      variant,
+      size,
+      loading = false,
+      asChild = false,
+      ...props
+    },
+    ref
+  ) => {
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        disabled={loading}
         {...props}
-      />
+      >
+        {loading && <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />}
+        {children}
+      </Comp>
     );
   }
 );
