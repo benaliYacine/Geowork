f4f7ba8a72b615b44b1138f01b153f1e2161e5c2
ilyes <132101833+ILYESBOUAZIZ@users.noreply.github.com>
2024-05-20 19:09:34 +0100

    m

--- src/pages/ExpertProposalPage.jsx
+++ src/pages/ExpertProposalPage.jsx
@@ -47,6 +47,7 @@ export default function SubmitProposal() {
         resolver: zodResolver(proposalSchema),
         defaultValues: {
             budget: budget,
+            coverLetter: coverLetter,
         },
     });
 
